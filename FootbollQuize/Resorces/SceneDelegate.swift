05228a72ef144331e758d75fe70ca0b260a31c84
Nikita318-creator <rharhunw@gmail.com>
2025-11-22 04:42:58 +0300

    last with OLD tabbar

--- FootbollQuize/Resorces/SceneDelegate.swift
+++ FootbollQuize/Resorces/SceneDelegate.swift
@@ -10,14 +10,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         self.window = UIWindow(windowScene: windowScene)
         
-        // 1. Создаем Splash Controller
         let splashVC = SplashVC()
         
-        // 2. Настраиваем кложур, который будет вызван после задержки
         splashVC.actionOnDismiss = { [weak self] in
             guard let self = self else { return }
             
-            // Плавная смена корневого контроллера
             UIView.transition(with: self.window!,
                               duration: 0.5,
                               options: .transitionCrossDissolve,
@@ -26,28 +23,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             }, completion: nil)
         }
         
-        // 3. Устанавливаем SplashVC как корневой контроллер окна
         self.window?.rootViewController = splashVC
         self.window?.overrideUserInterfaceStyle = .light
         self.window?.makeKeyAndVisible()
     }
     
-    // Приватный метод для создания основного Tab Bar Controller
     private func createMainTabBar() -> UITabBarController {
         
-        // 1. Создание экземпляров контроллеров
         let homeVC = HomeVC()
         let personalTrainingVC = PersonalTrainingVC()
         let progressVC = ProgressVC()
         
-        // 2. Оборачивание каждого контроллера в UINavigationController
         let homeNavController = UINavigationController(rootViewController: homeVC)
         let personalTrainingNavController = UINavigationController(rootViewController: personalTrainingVC)
         let progressNavController = UINavigationController(rootViewController: progressVC)
 
-        // 3. Настройка Tab Bar Items (названия и иконки)
-        // Важно: Иконки должны быть загружены как .alwaysTemplate, чтобы tintColor работал
-                
         homeVC.title = "Home"
         homeNavController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "homeTab")?.withRenderingMode(.alwaysTemplate), tag: 0)
 
@@ -65,11 +55,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             progressNavController
         ]
         
-        // -----------------------------------------------------------------
-        //  ✅ ИСПРАВЛЕННАЯ НАСТРОЙКА ЦВЕТОВ и ФОНА (через Appearance)
-        // -----------------------------------------------------------------
-        
-        let activeColor = UIColor.activeColor // Цвет для активного таба
+        let activeColor = UIColor.activeColor
         let appearance = UITabBarAppearance()
         appearance.backgroundColor = .primary
         appearance.stackedLayoutAppearance.selected.iconColor = activeColor
