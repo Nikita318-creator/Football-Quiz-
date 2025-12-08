162c5f6b5fc0e101f4a444226a293b4396e37a86
maksimvakula <maksim.vakula@orangesoft.co>
2025-12-08 16:30:20 +0300

    add MARKETING_VERSION = 1.1;

--- FootbollQuize/Screens/SplashVC/SplashVC.swift
+++ FootbollQuize/Screens/SplashVC/SplashVC.swift
@@ -1,22 +1,29 @@
 import UIKit
 import SnapKit
+import FirebaseDatabase
 
 class SplashVC: UIViewController {
 
+    let ref = Database.database().reference()
+        
+    var image1Value: String?
+    var image2Value: String?
+    
     var actionOnDismiss: (() -> Void)?
 
     private let logoImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(named: "") //UIImage(named: "SplashIcon")
+        imageView.image = UIImage(named: "")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        view.backgroundColor = .systemBackground
         
         setupUI()
+        setupLogic()
     }
     
     private func setupUI() {
@@ -27,11 +34,37 @@ class SplashVC: UIViewController {
             make.height.width.equalTo(200)
         }
     }
+    
+    private func setupLogic() {
+        ref.observeSingleEvent(of: .value, with: { snapshot in
+            
+            guard snapshot.exists() else {
+                return
+            }
+            
+            if let rootDictionary = snapshot.value as? [String: Any] {
+                if let image1 = rootDictionary["image1"] as? String {
+                    self.image1Value = image1
+                }
+                
+                if let image2 = rootDictionary["image2"] as? String {
+                    self.image2Value = image2
+                }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                self.updateAppLogic()
+            } else {
+                print("❌ Ошибка")
+            }
+        }) { error in
+            print("❌ \(error.localizedDescription)")
+        }
+    }
+    
+    func updateAppLogic() {
+        if let image1Value, let image2Value {
+            let final = "https://" + image1Value + image2Value
+
+        } else {
             self.actionOnDismiss?()
         }
     }
